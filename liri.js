1ccae6353650be90b31dc1470885e36565b71a64
Natalie Thompson <nataliethompson@Keanons-MBP.att.net>
2017-07-24 00:04:15 -0500

    Functioning app

--- liri.js
+++ liri.js
@@ -1,14 +1,35 @@
+
 var secretKeys = require("./keys.js");
 var Twitter = require('twitter');
-var value = process.argv[3];
+var value1= process.argv[3];
+var value2= process.argv[3];
 var request = require("request");
 
+var Spotify = require('node-spotify-api')
 
+
+
+
+
+
+var stringers = function (){
+  if (process.argv.length>3)
+  {
+  for(var i=4; i< process.argv.length; i++)
+  
+    {value1 = value1 + process.argv[i]+" ";
+    value2 = value2 + "+" + process.argv[i];
+  };
+  }
+  else {
+    return value1}
+  
+};
+
+stringers();
 
 function myTwitter(){
-
 var secrets = secretKeys.twitterKeys
- 
 var myTwitt = new Twitter({
   consumer_key: secrets.consumer_key,
   consumer_secret: secrets.consumer_secret,
@@ -29,7 +50,7 @@ myTwitt.get('statuses/user_timeline.json', params, function(error, tweets, respo
 
 
 
-var Spotify = require('node-spotify-api')
+
  
 function mySpotify(){
   var diff = new Spotify({
@@ -37,7 +58,7 @@ function mySpotify(){
   secret: secretKeys.spotifyKeys.client_secret
 });
  
-diff.search({ type: 'track', query: value }, function(err, data) {
+diff.search({ type: 'track', query: value1 }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
@@ -51,17 +72,26 @@ console.log(tpls.album.name);
  }
 });
 }
+// mySpotify();
 
 
 
 
 
 function myMovie () {
-request("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+request("http://www.omdbapi.com/?t=" + value2 + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
  
   if (!error && response.statusCode === 200) {
   var movieStats = JSON.parse(body); 
+foreach(movieStats.Ratings)
+{if (movieStats.Ratings[1].Source.includes("Rotten Tomatoes")){
+
+  
+}
+
+}
+
     // console.log(movieStats);
     console.log(movieStats.Title);
     console.log(movieStats.Year);
@@ -75,3 +105,4 @@ request("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=40e9cece",
   
 });
 };
+myMovie();
